de10d22dcdeed5dd3ae06f31ed6205d03f4ed8b2
pankajsewalia <pankajs@gyansha.com>
2014-06-03 16:28:24 +0530

    admin module added in easylearing

---
+++ gyansha_master/search.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>search result</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="fonts/font-awesome.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="search/search.js"></script>
+<script src="js/jquery.mobilemenu.js"></script> 
+<script src="js/jquery.ui.totop.js"></script>
+<!--[if lt IE 9]>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]><script src="../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header class="indent2">
+    <div class="container">
+        <div class="row">
+            <article class="col-lg-12 col-md-12 col-sm-12">
+                <div class="clearfix">
+                    <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img src="img/logo.png" alt=""></a></h1>
+                    <div class="menuBox clearfix">
+                        <ul class="follow_icon">
+                            <li><a href="#"><img src="img/follow_icon1.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon2.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon3.png" alt=""></a></li>
+                        </ul>
+                        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+                            <ul class="nav sf-menu clearfix">
+                                <li class="sub-menu"><a href="index.html">home<span></span></a><em></em>
+                                    <ul class="submenu">
+                                        <li><a href="#">about</a></li>
+                                        <li><a href="#">News</a></li>
+                                        <li><a href="#">services</a></li>
+                                        <li class="tr"></li>
+                                    </ul>
+                                </li>
+                                <li><a href="index-1.html">about us</a><em></em></li>
+                                <li><a href="index-2.html">services</a><em></em></li>
+                                <li><a href="index-3.html">programs</a><em></em></li>
+                                <li><a href="index-4.html">contacts</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</header>
+<!--content-->
+<div class="global indent">
+<div class="container">
+    <div class="row">
+        <section class="searchBox clearfix">
+            <article class="col-lg-12 col-md-12 col-sm-12">
+                <h3>Search result:</h3>
+                <div id="search-results"></div>
+            </article>
+        </section>
+    </div>
+</div>
+</div>
+<!--footer-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <article class="col-lg-12 col-md-12 col-sm-12">
+                <div class="bord-none">
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="index-1.html">about us</a></li>
+                        <li><a href="index-2.html">services</a></li>
+                        <li><a href="index-3.html">programs</a></li>
+                        <li><a href="index-4.html">contacts</a></li>
+                    </ul>
+                    <p><em id="copyright-year"></em> &copy; <a href="index-5.html">Privacy Policy</a></p>
+                </div>
+            </article>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
